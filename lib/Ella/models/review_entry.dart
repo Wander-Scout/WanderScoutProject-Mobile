98289077d3b36dfb40cabfbc3f2f432bc0154708
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-12-01 19:54:45 +0700

    add infinite scroll for reviews

--- lib/Ella/models/review_entry.dart
+++ lib/Ella/models/review_entry.dart
@@ -4,34 +4,42 @@
 
 import 'dart:convert';
 
-List<ReviewEntry> reviewEntryFromJson(String str) => List<ReviewEntry>.from(json.decode(str).map((x) => ReviewEntry.fromJson(x)));
-
-String reviewEntryToJson(List<ReviewEntry> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
 class ReviewEntry {
-  String username;  // Updated from 'user' to 'username'
-  String reviewText;
-  int rating;
-  DateTime createdAt;
+  final int id;
+  final String username;
+  final String reviewText;
+  final int rating;
+  final DateTime createdAt;
 
   ReviewEntry({
+    required this.id,
     required this.username,
     required this.reviewText,
     required this.rating,
     required this.createdAt,
   });
 
-  factory ReviewEntry.fromJson(Map<String, dynamic> json) => ReviewEntry(
-        username: json["username"],  // Updated to match API response
-        reviewText: json["review_text"],
-        rating: json["rating"],
-        createdAt: DateTime.parse(json["created_at"]),
-      );
+  factory ReviewEntry.fromJson(Map<String, dynamic> json) {
+    return ReviewEntry(
+      id: json['id'] as int,
+      username: json['username'] as String,
+      reviewText: json['review_text'] as String,
+      rating: json['rating'] as int,
+      createdAt: DateTime.parse(json['created_at'] as String),
+    );
+  }
 
   Map<String, dynamic> toJson() => {
+        "id": id,
         "username": username,
         "review_text": reviewText,
         "rating": rating,
         "created_at": createdAt.toIso8601String(),
       };
+
+  static List<ReviewEntry> listFromJson(String str) =>
+      List<ReviewEntry>.from(json.decode(str).map((x) => ReviewEntry.fromJson(x)));
+
+  static String listToJson(List<ReviewEntry> data) =>
+      json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 }
